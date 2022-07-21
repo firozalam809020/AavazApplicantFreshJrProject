67402ffc5680d182f68805c006eb344ab4590826
firozalam809020 <42115534+firozalam809020@users.noreply.github.com>
2022-07-22 01:34:12 +0530

    Update User.java

--- src/main/java/biz/aavaz/aavazapplicantfreshjrproject/model/User.java
+++ src/main/java/biz/aavaz/aavazapplicantfreshjrproject/model/User.java
@@ -5,5 +5,36 @@ package biz.aavaz.aavazapplicantfreshjrproject.model;
 //!!! https://sites.google.com/site/gson/gson-user-guide#TOC-JSON-Field-Naming-Support (JSON Field Naming Support)
 
 public class User {
+    private String firstname;
+    private String lastname;
+    private int age;
+
+    public String getFirstName() {
+        return firstname;
+    }
+
+    public void setFirstName() {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastname;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User [firstname=" + firstname + ", lastname=" + lastname + ", age=" + age + "]";
     
 }
