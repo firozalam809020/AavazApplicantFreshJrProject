bf44e418a0dac355aaeaaeb343aec5109ea5800e
firozalam809020 <42115534+firozalam809020@users.noreply.github.com>
2022-07-21 20:22:59 +0530

    Update User.java

--- src/main/java/biz/aavaz/aavazapplicantfreshjrproject/model/User.java
+++ src/main/java/biz/aavaz/aavazapplicantfreshjrproject/model/User.java
@@ -5,5 +5,37 @@ package biz.aavaz.aavazapplicantfreshjrproject.model;
 //!!! https://sites.google.com/site/gson/gson-user-guide#TOC-JSON-Field-Naming-Support (JSON Field Naming Support)
 
 public class User {
+    private int id;
+    private String name;
     
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "Skill [id=" + id + ", name=" + name + ", level=" + level + "]";
+    }
+
 }
